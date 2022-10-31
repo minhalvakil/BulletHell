954ccaa948bcca9e594160ab49470777ed644108
Minhal Vakil <minhal@vakil.ca>
2022-10-31 10:34:20 -0400

    Bullet Base Code

--- BulletHell/Assets/ShipController.cs
+++ BulletHell/Assets/ShipController.cs
@@ -7,6 +7,7 @@ public class ShipController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float speed = 5f;
     [SerializeField] float braking = 0f;
+    [SerializeField] GameObject bullet;
     float h, v;
     Rigidbody2D body;
     void Start()
@@ -19,6 +20,15 @@ public class ShipController : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 direction = this.transform.position - Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            direction.z = 0;
+            GameObject b = Instantiate(bullet, this.transform.position + direction.normalized *-1, this.transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = direction.normalized * -1 * b.GetComponent<Bullet>().speed;
+            b.GetComponent<Bullet>().owner = this.gameObject;
+        }
+        
     }
 
     private void FixedUpdate()
