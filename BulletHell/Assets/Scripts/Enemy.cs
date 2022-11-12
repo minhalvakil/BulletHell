5b27a17e78d951764e06bf76a1ee337cbb7f7e77
Minhal Vakil <minhal@vakil.ca>
2022-11-12 17:02:58 -0500

    Implemented Different Enemy types

--- BulletHell/Assets/Scripts/Enemy.cs
+++ BulletHell/Assets/Scripts/Enemy.cs
@@ -8,79 +8,101 @@ public class Enemy : Entity
     [SerializeField]
     GameObject bullet;
     [SerializeField]
-    float moveSpeed, cooldown;
+    float moveSpeed, cooldown, wanderTime;
     float timer;
+    float wanderTimer;
     [SerializeField]
     float seekRange;
-    [SerializeField]
-    float retreatRange;
     public behaviorState behavior;
+    public EnemyType enemyType;
     
 
     // Start is called before the first frame update
     void Start()
     {
         timer = Random.value * cooldown;
-        behavior = behaviorState.Seeking;
+        behavior = behaviorState.Approach;
         body = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dist = (this.transform.position - FindObjectOfType<ShipController>().transform.position);
-        if (behavior == behaviorState.Seeking)
-        {
-            this.transform.position += dist.normalized * Time.deltaTime * moveSpeed * -1;
-            if (dist.magnitude <= seekRange && dist.magnitude >= retreatRange)
-            {
-                behavior = behaviorState.Set;
-            }
-            else if(dist.magnitude < retreatRange)
-            {
-                behavior = behaviorState.Retreating;
-            }
-        }
-        else if (behavior == behaviorState.Retreating)
-        {
-            this.transform.position += dist.normalized * Time.deltaTime * moveSpeed;
-            if (dist.magnitude <= seekRange && dist.magnitude >= retreatRange)
-            {
-                behavior = behaviorState.Set;
-            }
-            else if (dist.magnitude > seekRange)
-            {
-                behavior = behaviorState.Seeking;
-            }
-        }
-        else if (behavior == behaviorState.Set)
-        {
-            if (dist.magnitude > seekRange)
-            {
-                behavior = behaviorState.Seeking;
-            }
-            else if (dist.magnitude < retreatRange)
-            {
-                behavior = behaviorState.Retreating;
-            }
-        }
+        Move();
         timer += Time.deltaTime;
         if(timer > cooldown)
         {
             timer = 0;
-            GameObject b = Instantiate(bullet, this.transform.position + dist.normalized * -1, this.transform.rotation);
-            b.GetComponent<Rigidbody2D>().velocity = dist.normalized * -1 * b.GetComponent<Bullet>().speed;
-            b.GetComponent<Bullet>().owner = this.gameObject;
+            Shoot();
         }
+        ClampToScreen();
+    }
+    void Move()
+    {
+        Vector3 dist = (-this.transform.position + FindObjectOfType<ShipController>().transform.position);
+        switch (behavior)
+        {
+            case behaviorState.Approach:
+                this.body.velocity = dist.normalized * moveSpeed;
+                break;
+            case behaviorState.Circle:
+                this.body.velocity = Vector3.Cross(dist, Vector3.forward).normalized * moveSpeed;
+                break;
+            case behaviorState.Wander:
+                if (wanderTimer > wanderTime)
+                {
+                    this.body.velocity = Random.insideUnitCircle.normalized * moveSpeed;
+                    wanderTimer = 0;
+                }
+                wanderTimer += Time.deltaTime;
+                break;
+        }
+        CheckState();
+    }
+    void CheckState()
+    {
+        if ((this.transform.position - FindObjectOfType<ShipController>().transform.position).magnitude < seekRange)
+        {
+            switch (enemyType)
+            {
+                case EnemyType.Standard:
+                    behavior = behaviorState.Wander;
+                    break;
+                default:
+                    behavior = behaviorState.Circle;
+                    break;
+            }
+        }
+        else
+        {
+            behavior = behaviorState.Approach;
+        }
+        
+    }
+    void Shoot()
+    {
+        Vector3 dist = (-this.transform.position + FindObjectOfType<ShipController>().transform.position);
+        float angle = (Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg)+180f;
+        GameObject b = Instantiate(bullet, this.transform.position + dist.normalized, Quaternion.AngleAxis(angle, Vector3.forward));
+       b.GetComponent<Rigidbody2D>().velocity = dist.normalized * b.GetComponent<Bullet>().speed;
+       b.GetComponent<Bullet>().owner = this.gameObject;
+    }
+    void ClampToScreen()
+    {
         Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
         Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 0.5f, maxScreenBounds.x - 0.5f), Mathf.Clamp(transform.position.y, minScreenBounds.y + 0.5f, maxScreenBounds.y - 0.5f), transform.position.z);
-
     }
     public enum behaviorState
     {
-        Seeking,
-        Set,
-        Retreating
+        Approach,
+        Circle,
+        Wander
+    }
+    public enum EnemyType
+    {
+        Standard,
+        Slash,
+        Homing
     }
 }
