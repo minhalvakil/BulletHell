1f03e36c3b307ea4c0b910e9e253f2731af684fe
minhalvakil <minhal@vakil.ca>
2022-11-19 18:54:36 -0500

    Wave System Implemented

--- BulletHell/Assets/Scripts/Wave.cs
+++ BulletHell/Assets/Scripts/Wave.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Wave : MonoBehaviour
 {
     public List<GameObject> enemyList;
-    public List<int> enemyRatios;
+    public List<float> enemyPercentages;
     public List<GameObject> spawnLocations;
     public int time, maxNumberOfEnemies, numberOfEnemies;
+    public float timeBetweenSpawns;
     public bool hasBoss;
     public GameObject boss;
 }
