eec947e77af44f14f3be6dd95dcd991657d5f841
Rectone472 <117608981+Rectone472@users.noreply.github.com>
2022-11-20 21:25:07 -0500

    stuff

--- BulletHell/Assets/Scripts/MainMenu.cs
+++ BulletHell/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        FindObjectOfType<AudioManager>().Play("Click");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void BackToMenu()
     {
+        FindObjectOfType<AudioManager>().Play("Click");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 
